a0ceb941f1537b6c86a740052eb9068fda2f1678
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-07-06 16:51:22 +0900

    💥 breaking: (prettier) bump up Prettier to V3
    Closes #10

---
+++ .changeset/famous-windows-call.md
@@ -0,0 +1,5 @@
+---
+'@x7ddf74479jn5/prettier-config-x7ddf74479jn5': major
+---
+
+💥 breaking: bump Prettier to V3
---
+++ packages/prettier/test/.prettierrc.js
@@ -0,0 +1,13 @@
+// @ts-check
+'use strict';
+
+const config = require('@x7ddf74479jn5/prettier-config-x7ddf74479jn5');
+
+/** @type {import('prettier').Config} */
+module.exports = {
+  ...config,
+  trailingComma: 'es5',
+  tabWidth: 4,
+  semi: false,
+  singleQuote: false,
+};
