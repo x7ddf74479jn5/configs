2f06a927b99d8d3446194f9a4cd66e41f9b55018
T.U <68179184+x7ddf74479jn5@users.noreply.github.com>
2023-07-06 17:08:41 +0900

    Feature/prettier (#11)
    * 💥 breaking: (prettier) bump up Prettier to V3
    
    Closes #10
    
    * ⬆️ up: bump prettier to v3

---
+++ packages/prettier/test/.prettierrc.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+'use strict';
+
+import config from '@x7ddf74479jn5/prettier-config-x7ddf74479jn5';
+
+/** @type {import('prettier').Config} */
+export default {
+  ...config,
+  trailingComma: 'es5',
+  tabWidth: 4,
+  semi: false,
+  singleQuote: false,
+};
---
+++ packages/prettier/test/case/index.ts
@@ -0,0 +1,8 @@
+export function add(a:number, b:number) {
+  return a + b;
+}
+
+const obj = {
+  key1: '',
+  key2: '',
+};
